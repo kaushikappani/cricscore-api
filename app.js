4930e88b7af04492edd0d69dcbbd4a246ebbb88b
info <58048398+kaushikappani@users.noreply.github.com>
2021-07-03 14:19:05 +0530

    more_data_schedule

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
 const express = require("express");
 const ejs = require("ejs");
-var cors = require('cors')
-const { fetch_recent, fetch_live, match_data, fetch_schedule, fetch_news, fetch_article } = require("./scrapper");
+var cors = require("cors");
+const rssToJson = require("rss-in-json");
+const {
+  fetch_recent,
+  fetch_live,
+  match_data,
+  fetch_schedule,
+  fetch_news,
+  fetch_article,
+} = require("./scrapper");
 const app = express();
-app.use(express.static('public'))
+app.use(express.static("public"));
 var corsOptions = {
-    origin: 'https://cricketscores.netlify.app',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.use(cors(corsOptions))
-app.set('view engine', 'ejs')
+  origin: "https://cricketscores.netlify.app",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.send({
-        message: "test"
-    })
-})
-app.get('/recent', async (req, res) => {
-    console.log(req.headers.host)
-    const data = await fetch_recent()
-    res.send(data)
-})
-app.get('/live', async (req, res) => {
-    const data = await fetch_live()
-    res.send(data)
-})
-app.get("/live/match", async (req, res) => {
-    const data = await match_data(req.query.url)
-    res.send(data)
-})
-app.get('/schedule', async (req, res) => {
-    const data = await fetch_schedule()
-    res.send(data)
-})
-app.get('/news', async (req, res) => {
-    const data = await fetch_news()
-    res.send(data)
-})
-app.get('/article', async (req, res) => {
-    console.log(req.query.url)
-    const data = await fetch_article(`https://www.cricbuzz.com/${req.query.url}`)
-    res.send(data)
-})
-app.get('*', (req, res) => {
-    res.send({ error: "wrong ur;" })
+  res.send({
+    message: "test",
+  });
 });
-const port = process.env.PORT || 5000
+app.get("/recent", async (req, res) => {
+  console.log(req.headers.host);
+  const data = await fetch_recent();
+  res.send(data);
+});
+app.get("/live", async (req, res) => {
+  const data = await fetch_live();
+  res.send(data);
+});
+app.get("/live/match", async (req, res) => {
+  const data = await match_data(req.query.url);
+  res.send(data);
+});
+app.get("/schedule", async (req, res) => {
+  const data = await fetch_schedule();
+  res.send(data);
+});
+app.get("/news", async (req, res) => {
+  const data = await fetch_news();
+  res.send(data);
+});
+app.get("/article", async (req, res) => {
+  console.log(req.query.url);
+  const data = await fetch_article(`https://www.cricbuzz.com/${req.query.url}`);
+  res.send(data);
+});
+
+app.get("*", (req, res) => {
+  res.send({ error: "wrong ur;" });
+});
+const port = process.env.PORT || 5000;
 app.listen(port, (req, res) => {
-    console.log("server started")
-})
+  console.log("server started");
+});
--- scrapper.js
+++ scrapper.js
@@ -3,133 +3,185 @@ const cherrio = require("cheerio");
 const { text } = require("cheerio/lib/api/manipulation");
 
 const html_fetch = async (url) => {
-    const response = await axios.get(url)
-    return response.data
+  const response = await axios.get(url);
+  return response.data;
 };
 
 const fetch_recent = async () => {
-    let data = []
-    const url = "https://www.cricbuzz.com/cricket-match/live-scores/recent-matches"
-    const html = await html_fetch(url)
-    const $ = cherrio.load(html);
-    $(".cb-mtch-lst").each((_, element) => {
-        const elem = $(element);
-        const title = elem.find("h3").text();
-        const date = elem.find("div.text-gray").html()
-        const url = "https://www.cricbuzz.com" + elem.find("h3 a").attr("href");
-        const scorecard_url = "https://www.cricbuzz.com" + elem.find("nav a[title='Scorecard']").attr("href")
-        const meta_text = elem.find("div.text-gray").text()
-        const score = elem.find(".cb-lv-scrs-well-complete").text()
-        data.push({
-            title, meta_text, score, url, scorecard_url, date
-        })
-    })
-    return data
-
-}
+  let data = [];
+  const url =
+    "https://www.cricbuzz.com/cricket-match/live-scores/recent-matches";
+  const html = await html_fetch(url);
+  const $ = cherrio.load(html);
+  $(".cb-mtch-lst").each((_, element) => {
+    const elem = $(element);
+    const title = elem.find("h3").text();
+    const date = elem.find("div.text-gray").html();
+    const url = "https://www.cricbuzz.com" + elem.find("h3 a").attr("href");
+    const scorecard_url =
+      "https://www.cricbuzz.com" +
+      elem.find("nav a[title='Scorecard']").attr("href");
+    const meta_text = elem.find("div.text-gray").text();
+    const score = elem.find(".cb-lv-scrs-well-complete").text();
+    data.push({
+      title,
+      meta_text,
+      score,
+      url,
+      scorecard_url,
+      date,
+    });
+  });
+  return data;
+};
 const fetch_live = async () => {
-    let data = []
-    const url = "https://www.cricbuzz.com/cricket-match/live-scores"
-    const html = await html_fetch(url)
-    const $ = cherrio.load(html);
-    $(".cb-mtch-lst").each((_, element) => {
-        const elem = $(element);
-        const title = elem.find("h3").text();
-        const date = elem.find("div.text-gray").html()
-        const url = "https://www.cricbuzz.com" + elem.find("h3 a").attr("href");
-        const scorecard_url = "https://www.cricbuzz.com" + elem.find("nav a[title='Scorecard']").attr("href")
-        const meta_text = elem.find("div.text-gray").text()
-        const score = elem.find(".cb-lv-scrs-well-live").text() || elem.find(".cb-lv-scrs-well-complete").text() || elem.find(".cb-lv-scrs-well-preview").text()
-        data.push({
-            title, meta_text, score, url, scorecard_url, date
-        })
-    })
-    return data
-}
+  let data = [];
+  const url = "https://www.cricbuzz.com/cricket-match/live-scores";
+  const html = await html_fetch(url);
+  const $ = cherrio.load(html);
+  $(".cb-mtch-lst").each((_, element) => {
+    const elem = $(element);
+    const title = elem.find("h3").text();
+    const date = elem.find("div.text-gray").html();
+    const url = "https://www.cricbuzz.com" + elem.find("h3 a").attr("href");
+    const scorecard_url =
+      "https://www.cricbuzz.com" +
+      elem.find("nav a[title='Scorecard']").attr("href");
+    const meta_text = elem.find("div.text-gray").text();
+    const score =
+      elem.find(".cb-lv-scrs-well-live").text() ||
+      elem.find(".cb-lv-scrs-well-complete").text() ||
+      elem.find(".cb-lv-scrs-well-preview").text();
+    data.push({
+      title,
+      meta_text,
+      score,
+      url,
+      scorecard_url,
+      date,
+    });
+  });
+  return data;
+};
 const match_data = async (url) => {
-    let data = {}
-    const html = await html_fetch(url)
-    const $ = cherrio.load(html);
-    const title = $("h1").text()
-    const scores = $(".cb-col-scores").text();
-    const recent = $("div.cb-min-rcnt").text();
-    const mom = $("div.cb-mom-itm").text();
-    const meta = $("div.cb-nav-subhdr").text();
-    const summary = $("div.cb-min-stts").text();
-    data.summary = summary
-    data.title = title
-    data.meta = meta
-    let commentry = [];
-    $("p.cb-com-ln").each((_, elem) => {
-        commentry.push($(elem).text());
-    })
-    data.commentry = commentry;
-    data.recent = recent;
-    data.mom = mom
-    data.scores = scores;
-    let records = [];
-    $("div.cb-min-inf div.cb-col-100").each((i, element) => {
-        const elem = $(element);
-        let stats = []
-        elem.find("div.cb-col").each((_, e) => {
-            stats.push($(e).text())
-        })
-        records.push({ stats })
-        data.records = records
-    })
-    return data
-}
+  let data = {};
+  const html = await html_fetch(url);
+  const $ = cherrio.load(html);
+  const title = $("h1").text();
+  const scores = $(".cb-col-scores").text();
+  const recent = $("div.cb-min-rcnt").text();
+  const mom = $("div.cb-mom-itm").text();
+  const meta = $("div.cb-nav-subhdr").text();
+  const summary = $("div.cb-min-stts").text();
+  data.summary = summary;
+  data.title = title;
+  data.meta = meta;
+  let commentry = [];
+  $("p.cb-com-ln").each((_, elem) => {
+    commentry.push($(elem).text());
+  });
+  data.commentry = commentry;
+  data.recent = recent;
+  data.mom = mom;
+  data.scores = scores;
+  let records = [];
+  $("div.cb-min-inf div.cb-col-100").each((i, element) => {
+    const elem = $(element);
+    let stats = [];
+    elem.find("div.cb-col").each((_, e) => {
+      stats.push($(e).text());
+    });
+    records.push({ stats });
+    data.records = records;
+  });
+  return data;
+};
 const fetch_schedule = async () => {
-    let data = []
-    const html = await html_fetch("https://www.cricbuzz.com/cricket-schedule/upcoming-series/international")
-    const $ = cherrio.load(html)
-    $("div#international-list div.cb-col-67.cb-col").each((_, element) => {
-        const elem = $(element);
-        const title = elem.find("div div span[itemprop='name']").attr("content")
-        const date = elem.find("div div span[itemprop='startDate']").attr("content")
-        const time = elem.find("div.cb-mtchs-dy-tm").text()
-        const location = elem.find("div[itemprop='location']").text()
-        data.push({ title, date, time, location })
-    })
-    return data
-}
+  let data = [];
+  const html = await html_fetch(
+    "https://www.cricbuzz.com/cricket-schedule/upcoming-series/international"
+  );
+  const $ = cherrio.load(html);
+  const pagination_url = $("div.ajax-pagination").attr("url");
+  $("div#international-list div.cb-col-67.cb-col").each((_, element) => {
+    const elem = $(element);
+    const title = elem.find("div div span[itemprop='name']").attr("content");
+    const date = elem
+      .find("div div span[itemprop='startDate']")
+      .attr("content");
+    const time = elem.find("div.cb-mtchs-dy-tm").text();
+    const location = elem.find("div[itemprop='location']").text();
+    data.push({ title, date, time, location });
+  });
+  let more_data = await fetch_schedule_page(pagination_url);
+  return [...data, ...more_data];
+};
 const fetch_news = async () => {
-    let data = [];
-    const html = await html_fetch("https://www.cricbuzz.com/cricket-news/latest-news")
-    const $ = cherrio.load(html);
-    $("div.cb-nws-lft-col div.cb-pos-rel").each((_, element) => {
-        const elem = $(element);
-        let path = ""
-        if (elem.find("img").attr("src") !== undefined) {
-            path = elem.find("img").attr("src").split("205x152")[0] + "500x500" + elem.find("img").attr("src").split("205x152")[1]
-        }
-        if (elem.find("img").attr("source") !== undefined) {
-            path = elem.find("img").attr("source").split("205x152")[0] + "500x500" + elem.find("img").attr("source").split("205x152")[1]
-        }
-        const image = "https://www.cricbuzz.com" + path
-        const title = elem.find("h2").text();
-        const link = elem.find("a").attr("href");
-        const intro = elem.find("div.cb-nws-intr").text()
-        const time = elem.find("span.cb-nws-time").text()
-        data.push({ image, title, intro, time, link })
-    })
-    return data
-}
+  let data = [];
+  const html = await html_fetch(
+    "https://www.cricbuzz.com/cricket-news/latest-news"
+  );
+  const $ = cherrio.load(html);
+  $("div.cb-nws-lft-col div.cb-pos-rel").each((_, element) => {
+    const elem = $(element);
+    let path = "";
+    if (elem.find("img").attr("src") !== undefined) {
+      path =
+        elem.find("img").attr("src").split("205x152")[0] +
+        "500x500" +
+        elem.find("img").attr("src").split("205x152")[1];
+    }
+    if (elem.find("img").attr("source") !== undefined) {
+      path =
+        elem.find("img").attr("source").split("205x152")[0] +
+        "500x500" +
+        elem.find("img").attr("source").split("205x152")[1];
+    }
+    const image = "https://www.cricbuzz.com" + path;
+    const title = elem.find("h2").text();
+    const link = elem.find("a").attr("href");
+    const intro = elem.find("div.cb-nws-intr").text();
+    const time = elem.find("span.cb-nws-time").text();
+    data.push({ image, title, intro, time, link });
+  });
+  return data;
+};
 const fetch_article = async (url) => {
-    const html = await html_fetch(url);
-    const $ = cherrio.load(html);
-    const title = $("h1[itemprop='headline']").text();
-    let body = [];
-    $("section[itemprop='articleBody']").each((_, e) => {
-        body.push($(e).text())
-    })
-    const subText = $("div.cb-nws-sub-txt").text()
-    const image = $("section[itemprop='image'] img").attr('src')
-    const date = $("time").attr('datetime')
-    let data = { title, body, image, subText, date: date.substring(0, 16) }
-    return data
+  const html = await html_fetch(url);
+  const $ = cherrio.load(html);
+  const title = $("h1[itemprop='headline']").text();
+  let body = [];
+  $("section[itemprop='articleBody']").each((_, e) => {
+    body.push($(e).text());
+  });
+  const subText = $("div.cb-nws-sub-txt").text();
+  const image = $("section[itemprop='image'] img").attr("src");
+  const date = $("time").attr("datetime");
+  let data = { title, body, image, subText, date: date.substring(0, 16) };
+  return data;
+};
 
-}
+fetch_schedule_page = async (url) => {
+  let data = [];
+  const html = await html_fetch(`https://www.cricbuzz.com/${url}`);
+  const $ = cherrio.load(html);
+  $("div.cb-col-67.cb-col").each((_, e) => {
+    const date = $(e)
+      .find("div div span[itemprop='startDate']")
+      .attr("content");
+    const title = $(e).find("span[itemprop='name']").attr("content");
+    const location = $(e).find("div[itemprop='location']").text();
+    const time = $(e).find("div.cb-mtchs-dy-tm").text();
+    let item = { date, title, location, time };
+    data.push(item);
+  });
+  return data;
+};
 module.exports = {
-    fetch_recent, fetch_live, match_data, fetch_schedule, fetch_news, fetch_article
-}
+  fetch_recent,
+  fetch_live,
+  match_data,
+  fetch_schedule,
+  fetch_news,
+  fetch_article,
+};
