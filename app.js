70956f6b116be2e14c0c09ad845958e38f7ec235
info <58048398+kaushikappani@users.noreply.github.com>
2021-07-04 00:13:43 +0530

    cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const {
 const app = express();
 app.use(express.static("public"));
 var corsOptions = {
-  origin: "https://cricketscores.netlify.app",
+  origin: ["https://cricketscores.netlify.app", "localhost:3000"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
